a2511bd20fb036743942c08d7fe3cf6dc5015c18
ierman1 <germanperezb98@gmail.com>
2025-08-21 20:08:31 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DefaultNavbar from './components/DefaultNavbar.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <DefaultNavbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer></footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/DefaultNavbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="px-4 py-2">
+        <ul class="flex space-x-4 almendra-regular text-2xl">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><ThemeToggle /></li>
+        </ul>
+    </nav>
+</template>
