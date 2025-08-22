e19b6fcd192a863ca90b1f82f87153c61117f350
ierman1 <germanperezb98@gmail.com>
2025-08-22 23:59:32 +0200

    Adds fontawesome icons

--- src/sections/FeaturesSection.vue
+++ src/sections/FeaturesSection.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTable, faPeopleGroup, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+</script>
+
 <template>
     <section class="py-16">
         <article class="container mx-auto px-4 lg:px-24 text-center">
@@ -9,18 +14,21 @@
             </p>
             <div class="flex flex-col md:flex-row mt-16">
                 <div class="flex-1 p-4">
+                    <FontAwesomeIcon :icon="faTable" class="text-6xl mb-4 text-teal-500" />
                     <h3 class="text-xl font-semibold mb-2">
                         Reserve a table today!
                     </h3>
                     <p>Secure your spot for an unforgettable gaming experience.</p>
                 </div>
                 <div class="flex-1 p-4">
+                    <FontAwesomeIcon :icon="faPeopleGroup" class="text-6xl mb-4 text-teal-500" />
                     <h3 class="text-xl font-semibold mb-2">
                         About our community
                     </h3>
                     <p>We foster a welcoming and inclusive environment for all tabletop gamers.</p>
                 </div>
                 <div class="flex-1 p-4">
+                    <FontAwesomeIcon :icon="faCalendarDays" class="text-6xl mb-4 text-teal-500" />
                     <h3 class="text-xl font-semibold mb-2">
                         Enjoy community events!
                     </h3>
