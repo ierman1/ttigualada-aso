4109cc28008a878f0bfbf43e0208a4fdae8ce543
ierman1 <germanperezb98@gmail.com>
2025-08-23 00:36:59 +0200

    Adds brand icons and a socials section

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,10 +2,12 @@
 import WelcomeSection from '../sections/WelcomeSection.vue'
 import FeaturesSection from '../sections/FeaturesSection.vue'
 import UpcomingEventsSection from '../sections/UpcomingEventsSection.vue'
+import SocialsSection from '../sections/SocialsSection.vue'
 </script>
 
 <template>
     <WelcomeSection />
     <FeaturesSection />
     <UpcomingEventsSection />
+    <SocialsSection />
 </template>
---
+++ src/sections/SocialsSection.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTwitter, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
+
+const socialLinks = ref([
+    { name: 'Twitter', url: 'https://twitter.com', icon: faTwitter },
+    { name: 'Instagram', url: 'https://instagram.com', icon: faInstagram },
+    { name: 'TikTok', url: 'https://tiktok.com', icon: faTiktok }
+])
+</script>
+
+<template>
+    <section class="py-16">
+        <article class="container mx-auto px-4 lg:px-24 text-center modern-antiqua-regular">
+            <h2 class="text-3xl md:text-5xl mb-4">
+                Follow Us
+            </h2>
+            <p class="text-lg">
+                Stay connected with us on social media.
+            </p>
+            <div class="flex justify-center space-x-4 mt-8">
+                <a 
+                    v-for="social in socialLinks"
+                    :key="social.name"
+                    :href="social.url"
+                    class="text-teal-500 text-4xl"
+                >
+                    <FontAwesomeIcon :icon="social.icon" />
+                </a>
+            </div>
+        </article>
+    </section>
+</template>
